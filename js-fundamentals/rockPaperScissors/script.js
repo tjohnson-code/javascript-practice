e97af45405a926fb5d50d2b5e63e06e1cf2c0b4c
Tim Johnson <tjjohnson.86@gmail.com>
2022-09-20 12:50:49 -0500

    added if statement for winner announcement

--- js-fundamentals/rockPaperScissors/script.js
+++ js-fundamentals/rockPaperScissors/script.js
@@ -12,24 +12,42 @@ function gameRPS() {
         btnPressed.textContent = 'Rock'
         let computerChoice = arrayRPS[Math.floor(Math.random() * arrayRPS.length)];
         let computerOutput = document.getElementById('computerChoice');
-        computerOutput.textContent = computerChoice
+        computerOutput.textContent = computerChoice;
         console.log(computerChoice);
-        resultRPS.textContent = 'testing';
+        if (computerChoice === 'Rock') {
+            resultRPS.textContent = 'Tie Game!'
+        }else if (computerChoice === 'Paper') {
+            resultRPS.textContent = 'You Lose!'
+        }else {
+            resultRPS.textContent = 'You Win!'
+        }
     });
     paperBtn.addEventListener('click', () => {
         btnPressed.textContent = 'Paper'
         let computerChoice = arrayRPS[Math.floor(Math.random() * arrayRPS.length)];
         let computerOutput = document.getElementById('computerChoice');
-        computerOutput.textContent = computerChoice
+        computerOutput.textContent = computerChoice;
         console.log(computerChoice);
-        resultRPS.textContent = 'testing2';
+        if (computerChoice === 'Paper') {
+            resultRPS.textContent = 'Tie Game!'
+        }else if (computerChoice === 'Scissors') {
+            resultRPS.textContent = 'You Lose!'
+        }else {
+            resultRPS.textContent = 'You Win!'
+        }
     });
     scissorsBtn.addEventListener('click', () => {
         btnPressed.textContent = 'Scissors'
         let computerChoice = arrayRPS[Math.floor(Math.random() * arrayRPS.length)];
         let computerOutput = document.getElementById('computerChoice');
-        computerOutput.textContent = computerChoice
+        computerOutput.textContent = computerChoice;
         console.log(computerChoice);
-        resultRPS.textContent = 'testing3';
+        if (computerChoice === 'Scissors') {
+            resultRPS.textContent = 'Tie Game!'
+        }else if (computerChoice === 'Rock') {
+            resultRPS.textContent = 'You Lose!'
+        }else {
+            resultRPS.textContent = 'You Win!'
+        }
     });
 }
