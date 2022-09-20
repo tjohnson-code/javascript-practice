d987c152ea12e3a76199ce61386c4d6296f62b25
Tim Johnson <tjjohnson.86@gmail.com>
2022-09-20 12:32:28 -0500

    fixed computer choices for Rock Paper Scissors and added filler text for winner announcement

--- js-fundamentals/rockPaperScissors/script.js
+++ js-fundamentals/rockPaperScissors/script.js
@@ -1,23 +1,35 @@
-const rockBtn = document.getElementById('rockBtn');
-const paperBtn = document.getElementById('paperBtn');
-const scissorsBtn = document.getElementById('scissorsBtn');
-const btnPressed = document.getElementById('playerChoice');
-const arrayRPS = ['Rock', 'Paper', 'Scissors'];
-let computerChoice = arrayRPS[Math.floor(Math.random() * arrayRPS.length)];
-let computerOutput = document.getElementById('computerChoice');
+gameRPS();
 
-rockBtn.addEventListener('click', () => {
-    btnPressed.textContent = 'Rock'
-    computerOutput.textContent = computerChoice
-    console.log(computerChoice);
-});
-paperBtn.addEventListener('click', () => {
-    btnPressed.textContent = 'Paper'
-    computerOutput.textContent = computerChoice
-    console.log(computerChoice);
-});
-scissorsBtn.addEventListener('click', () => {
-    btnPressed.textContent = 'Scissors'
-    computerOutput.textContent = computerChoice
-    console.log(computerChoice);
-});
+function gameRPS() {
+    const rockBtn = document.getElementById('rockBtn');
+    const paperBtn = document.getElementById('paperBtn');
+    const scissorsBtn = document.getElementById('scissorsBtn');
+    const btnPressed = document.getElementById('playerChoice');
+    const arrayRPS = ['Rock', 'Paper', 'Scissors'];
+    const resultRPS = document.getElementById('gameResult');
+
+    rockBtn.addEventListener('click', () => {
+        btnPressed.textContent = 'Rock'
+        let computerChoice = arrayRPS[Math.floor(Math.random() * arrayRPS.length)];
+        let computerOutput = document.getElementById('computerChoice');
+        computerOutput.textContent = computerChoice
+        console.log(computerChoice);
+        resultRPS.textContent = 'testing';
+    });
+    paperBtn.addEventListener('click', () => {
+        btnPressed.textContent = 'Paper'
+        let computerChoice = arrayRPS[Math.floor(Math.random() * arrayRPS.length)];
+        let computerOutput = document.getElementById('computerChoice');
+        computerOutput.textContent = computerChoice
+        console.log(computerChoice);
+        resultRPS.textContent = 'testing2';
+    });
+    scissorsBtn.addEventListener('click', () => {
+        btnPressed.textContent = 'Scissors'
+        let computerChoice = arrayRPS[Math.floor(Math.random() * arrayRPS.length)];
+        let computerOutput = document.getElementById('computerChoice');
+        computerOutput.textContent = computerChoice
+        console.log(computerChoice);
+        resultRPS.textContent = 'testing3';
+    });
+}
