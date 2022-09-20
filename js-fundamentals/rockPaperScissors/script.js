83fc8aab9eb73f8ad68cc2bd0994f27531c3b532
Tim Johnson <tjjohnson.86@gmail.com>
2022-09-20 14:46:50 -0500

    Styling and total game counter added. First attempt at RPS game complete.

--- js-fundamentals/rockPaperScissors/script.js
+++ js-fundamentals/rockPaperScissors/script.js
@@ -10,9 +10,11 @@ function gameRPS() {
     const playerWins = document.getElementById('playerWins');
     const computerWins = document.getElementById('computerWins');
     const ties = document.getElementById('ties');
+    const totalGames = document.getElementById('totalGames');
     let tieCount = 0;
     let winCount = 0;
     let loseCount = 0;
+    let gameCount = 0;
 
     rockBtn.addEventListener('click', () => {
         btnPressed.textContent = 'Rock'
@@ -22,16 +24,22 @@ function gameRPS() {
         console.log(computerChoice);
         if (computerChoice === 'Rock') {
             tieCount ++;
+            gameCount ++;
             resultRPS.textContent = 'Tie Game!'
             ties.textContent = tieCount;
+            totalGames.textContent = gameCount;
         }else if (computerChoice === 'Paper') {
             loseCount ++;
+            gameCount ++;
             resultRPS.textContent = 'You Lose!'
             computerWins.textContent = loseCount;
+            totalGames.textContent = gameCount;
         }else {
             winCount ++;
+            gameCount ++;
             resultRPS.textContent = 'You Win!'
             playerWins.textContent = winCount;
+            totalGames.textContent = gameCount;
         }
     });
     paperBtn.addEventListener('click', () => {
@@ -42,16 +50,22 @@ function gameRPS() {
         console.log(computerChoice);
         if (computerChoice === 'Paper') {
             tieCount ++;
+            gameCount ++;
             resultRPS.textContent = 'Tie Game!'
             ties.textContent = tieCount;
+            totalGames.textContent = gameCount;
         }else if (computerChoice === 'Scissors') {
             loseCount ++;
+            gameCount ++;
             resultRPS.textContent = 'You Lose!'
             computerWins.textContent = loseCount;
+            totalGames.textContent = gameCount;
         }else {
             winCount ++;
+            gameCount ++;
             resultRPS.textContent = 'You Win!'
             playerWins.textContent = winCount;
+            totalGames.textContent = gameCount;
         }
     });
     scissorsBtn.addEventListener('click', () => {
@@ -62,16 +76,22 @@ function gameRPS() {
         console.log(computerChoice);
         if (computerChoice === 'Scissors') {
             tieCount ++;
+            gameCount ++;
             resultRPS.textContent = 'Tie Game!'
             ties.textContent = tieCount;
+            totalGames.textContent = gameCount;
         }else if (computerChoice === 'Rock') {
             loseCount ++;
+            gameCount ++;
             resultRPS.textContent = 'You Lose!'
             computerWins.textContent = loseCount;
+            totalGames.textContent = gameCount;
         }else {
             winCount ++;
+            gameCount ++;
             resultRPS.textContent = 'You Win!'
             playerWins.textContent = winCount;
+            totalGames.textContent = gameCount;
         }
     });
 }
